0ee02f09350f2a43b67b2c8f31c721ed7dfba2b8
Мария <maryreznik82@gmail.com>
2022-09-20 00:04:17 +0400

    Добавляет шлифовку по пиксель перфект

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,15 @@
 let navMain = document.querySelector('.main-header__nav');
 let navToggle = document.querySelector('.main-header__nav-toggle');
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-header__nav--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed')
-    navMain.classList.add('main-nav--opened')
+  if (navMain.classList.contains('main-header__nav--closed')) {
+    navMain.classList.remove('main-header__nav--closed')
+    navMain.classList.add('main-header__nav--opened')
 
   } else {
-    navMain.classList.add('main-nav--closed')
-    navMain.classList.remove('main-nav--opened')
+    navMain.classList.add('main-header__nav--closed')
+    navMain.classList.remove('main-header__nav--opened')
   }
 })
